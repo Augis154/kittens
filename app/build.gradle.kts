9e7e65686a6c6e22376fd7a586f5b008bbdd4711
Augis <augustas.stankevicius@tuta.io>
2026-09-06 13:17:08 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    // Apply the application plugin to add support for building a CLI application in Java.
+    application
+}
+
+repositories {
+    // Use Maven Central for resolving dependencies.
+    mavenCentral()
+}
+
+dependencies {
+    // This dependency is used by the application.
+    implementation(libs.guava)
+}
+
+// Apply a specific Java toolchain to ease working on different environments.
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+application {
+    // Define the main class for the application.
+    mainClass = "kittens.client.Main"
+}
+
+// Custom task to run the Client
+tasks.register<JavaExec>("runClient") {
+    group = "application"
+    mainClass = "kittens.client.Main"
+    classpath = sourceSets["main"].runtimeClasspath
+}
+
+// Custom task to run the Server
+tasks.register<JavaExec>("runServer") {
+    group = "application"
+    mainClass = "kittens.server.Main"
+    classpath = sourceSets["main"].runtimeClasspath
+    standardInput = System.`in` // Allows you to type commands into the server console
+}
---
+++ app/src/main/java/kittens/server/Main.java
@@ -0,0 +1,7 @@
+package kittens.server;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello, Server!");
+  }
+}
